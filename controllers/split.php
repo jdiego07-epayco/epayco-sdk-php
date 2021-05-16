38f23b2dce42a7506f159704ba940367d979ba1d
epayco-jdiego07 <juan.vargas.epayco@gmail.com>
2021-05-15 19:41:56 -0500

    Actualización de tipo de dato en variable split_receivers

--- controllers/split.php
+++ controllers/split.php
@@ -1,6 +1,6 @@
 <?php
 
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
 $epayco = new Epayco\Epayco(array(
      "apiKey" => "55b75fd6b1ef17eaf394fa985de6563c", 
      "privateKey" => "9327203f56866f1ed1ef4f56272ee771", 
@@ -8,28 +8,30 @@ $epayco = new Epayco\Epayco(array(
      "test" => false
  ));
 
-$token = $epayco->token->create(array(
-    "card[number]" => '5240521756556621',
-    "card[exp_year]" => "2027",
-    "card[exp_month]" => "02",
-    "card[cvc]" => "049"
-));
+// $token = $epayco->token->create(array(
+//     "card[number]" => '5240521756556621',
+//     "card[exp_year]" => "2027",
+//     "card[exp_month]" => "02",
+//     "card[cvc]" => "049"
+// ));
 
-$customer = $epayco->customer->create(array(
-    "token_card" => $token->id,
-    "name" => "Juan Diego",
-    "last_name" => "Vargas Posada", 
-    "email" => "diego.vargas@payco.co",
-    "default" => false,
-    "city" => "Medellin",
-    "address" => "Cl 104 # 74a - 4",
-    "phone" => "5502712",
-    "cell_phone" => "3042462218",
-));
+// $customer = $epayco->customer->create(array(
+//     "token_card" => $token->id,
+//     "name" => "Juan Diego",
+//     "last_name" => "Vargas Posada", 
+//     "email" => "diego.vargas@payco.co",
+//     "default" => false,
+//     "city" => "Medellin",
+//     "address" => "Cl 104 # 74a - 4",
+//     "phone" => "5502712",
+//     "cell_phone" => "3042462218",
+// ));
 
 $pay = $epayco->charge->create(array(
-    "token_card" => $token->id,
-    "customer_id" => $customer->data->customerId,
+    // "token_card" => $token->id,
+    // "customer_id" => $customer->data->customerId,
+    "token_card" => "0837e22f054270493229d03",
+    "customer_id" => "0754200c0e6dd165e4c5344",
     "doc_type" => "CC",
     "doc_number" => "1194418306",
     "name" => "Juan Diego",
@@ -37,7 +39,7 @@ $pay = $epayco->charge->create(array(
     "email" => "diego.vargas@payco.co",
     "bill" => strval(rand(500000, 300000)),
     "description" => "SDK PHP Pruebas ePayco Split TC",
-    "value" => "6",
+    "value" => "1",
     "tax" => "0",
     "tax_base" => "0",
     "currency" => "COP",
@@ -52,13 +54,18 @@ $pay = $epayco->charge->create(array(
     "splitpayment" => "true",
     "split_app_id" => "515360",
     "split_merchant_id" => "515360",
-    "split_type" => "02",
+    "split_type" => "01",
     "split_primary_receiver" => "515360",
     "split_primary_receiver_fee" => "0",
     "split_rule" => 'multiple', // Parametros para split multiple
-    "split_receivers" => json_encode(array(
-        array('id' => '9898', 'total' => '6', 'iva' => '0', 'base_iva' => '6', 'fee' => '50')
-    )),// Parametros para split multiple
+    "split_receivers" => array(
+        array('id' => '93006', 'total' => '1', 'iva' => '0', 'base_iva' => '1', 'fee' => '0')
+    ), // Parametros para split multiple
+//  "split_receivers" => json_encode(array(
+//      array('id' => 'P_CUST_ID_CLIENTE 1 RECEIVER', 'total' => '58000', 'iva' => '8000', 'base_iva' => '50000', 'fee' => '10'),
+//      array('id' => 'P_CUST_ID_CLIENTE 2 RECEIVER', 'total' => '58000', 'iva' => '8000', 'base_iva' => '50000', 'fee' => '10')
+//  )) // Campo obligatorio sí se envía el campo split_rule
+//  ));
     "use_default_card_customer" => false,
     "extras" => array(
         "extra1" => "",
